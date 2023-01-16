9cd77107960d9c84108d450f936855646d0e2b9a
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-16 17:57:08 +0530

    finding maximum number

--- Maximumnumberday13/src/com/bridgelabz/Maximumintegerday13.java
+++ Maximumnumberday13/src/com/bridgelabz/Maximumintegerday13.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
 public class Maximumintegerday13 {
-    public static void main(String[] args) {
-        
+
+        int checkMaximum (Integer x, Integer y, Integer z){
+            int max = x; //Assuming x is the largest (initially)
+
+            if (y.compareTo(max) > 0)
+                max = y; // y is the largest so far
+
+            if (z.compareTo(max) > 0)
+                max = z; // now z is the largest
+
+            return max;
+        }
+
+        public static void main (String[]args){
+            Maximumintegerday13 obj = new Maximumintegerday13();
+            System.out.println("The maximum integer is : " + obj.checkMaximum(1, 7, 5));
+
+
     }
 }
